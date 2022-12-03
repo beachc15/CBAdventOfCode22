a93022cad6990f865f1706ce3e3f58925f08efa6
beach <beachc15@gmail.com>
2022-12-03 00:54:27 -0500

    after AI editing. 0.00099611S

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,41 +1,49 @@
-def solve():
+import time
+
+def solve(part):
     score = 0
     with open(r"C:\Users\beach\PycharmProjects\CBAdventOfCode22\Day3\input", 'r') as f:
-        for line in f:
-            first_half, second_half = line[:len(line) // 2], line[len(line) // 2:]
-            for character in first_half:
-                if character in second_half:
-                    score += priority(character)
-                    break
-    return score
-
-
-def solve2():
-    sack_i, score = 0, 0
-    with open(r"C:\Users\beach\PycharmProjects\CBAdventOfCode22\Day3\input", 'r') as f:
-        chunks = f.read().split('\n')
-    while sack_i < len(chunks) - 1:
-        c1 = (chunks[sack_i])
-        c2 = (chunks[sack_i + 1])
-        c3 = (chunks[sack_i + 2])
-
-        for character in c1:
-            if c1.find(character) != -1 and c2.find(character) != -1 and c3.find(character) != -1:
-                score += priority(character)
-                break
-
-        sack_i += 3
+        if part == 1:
+            # Read the input file line by line
+            for line in f:
+                first_half = line[:len(line) // 2]
+                second_half = line[len(line) // 2:]
+                for character in first_half:
+                    if second_half.find(character) != -1:
+                        score += priority(character)
+                        break
+        elif part == 2:
+            # Read the first three lines of the file
+            c1 = f.readline().strip()
+            c2 = f.readline().strip()
+            c3 = f.readline().strip()
+            while c1 and c2 and c3:
+                for character in c1:
+                    if c2.find(character) != -1 and c3.find(character) != -1:
+                        score += priority(character)
+                        break
+                # Read the next three lines of the file
+                c1 = c2
+                c2 = c3
+                c3 = f.readline().strip()
 
     return score
 
 
 def priority(x):
-    li = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-          'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
-          'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    return li.index(x)
+    # The ASCII value of the lowercase letters is in the range 97-122,
+    # and the ASCII value of the uppercase letters is in the range 65-90.
+    if ord(x) >= 97:
+        return ord(x) - 96
+    elif ord(x) >= 65:
+        return ord(x) - 38
+    else:
+        return 0
 
 
 if __name__ == '__main__':
-    print(solve())
-    print(solve2())
+    start = time.time()
+    print(solve(1))
+    print(solve(2))
+    end = time.time()
+    print(end-start)
