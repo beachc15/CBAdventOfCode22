524f2a1833f7a68bf146d4dcbffe08692af52d6d
beach <beachc15@gmail.com>
2022-12-05 20:02:57 -0500

    correct solution: human

--- Day5/day5.py
+++ Day5/day5.py
@@ -1,32 +1,60 @@
-def solve(part: int) -> int:
+def solve(part: int):
     # boxes
     one = ['Z', 'P', 'M', 'H', 'R']
     two = ['P', 'C', 'J', 'B']
-    pairs_count = 0
+    three = ['S', 'N', 'H', 'G', 'L', 'C', 'D']
+    four = ['F', 'T', 'M', 'D', 'Q', 'S', 'R', 'L']
+    five = ['F', 'S', 'P', 'Q', 'B', 'T', 'Z', 'M']
+    six = ['T', 'F', 'S', 'Z', 'B', 'G']
+    seven = ['N', 'R', 'V']
+    eight = ['P', 'G', 'L', 'T', 'D', 'V', 'C', 'M']
+    nine = ['W', 'Q', 'N', 'J', 'F', 'M', 'L']
+    boxes = [one, two, three, four, five, six, seven, eight, nine]
+
+    top_row = [None]
     with open("input", 'r') as f:
         for line in f:
-            a1, a2 = get_elf_assignments(line)
-            if part == 1:
-                if all(x in a1 for x in a2) or all(x in a2 for x in a1):
-                    pairs_count += 1
-            elif part == 2:
-                if any(x in a1 for x in a2) or any(x in a2 for x in a1):
-                    pairs_count += 1
-    return pairs_count
+            amount, start, end = parse_direction(line)
+            boxes_to_move = boxes[start][-amount:]
+            del boxes[start][-amount:]
+            # copy the list
+            flipped_boxes_to_move = boxes_to_move.copy()
+            # flip the order
+            flipped_boxes_to_move.reverse()
+            # place boxes on new stack
+            if part == 9000:
+                boxes[end].extend(flipped_boxes_to_move)
+            if part == 9001:
+                boxes[end].extend(boxes_to_move)
+            top_row = get_top_row(boxes)
+
+    return ''.join(top_row)
 
 
-def get_elf_assignments(raw_string: str) -> tuple[list[int], list[int]]:
-    # Split the string into a first and second assignment
-    assignment_one, assignment_two = raw_string.split(",")
-    assignment_one_start, assignment_one_end = list(map(int, assignment_one.split("-")))
-    assignment_two_start, assignment_two_end = list(map(int, assignment_two.split("-")))
-    # create two lists
-    assignment_one_range = list(range(assignment_one_start, assignment_one_end + 1))
-    assignment_two_range = list(range(assignment_two_start, assignment_two_end + 1))
+def get_top_row(stacks: list):
+    # Take in a list of letters, return the last letter or "top" of box stack
+    out_stack = []
+    for stack in stacks:
+        try:
+            out_stack.append(stack[-1])
+        except IndexError:
+            pass
+    return out_stack
 
-    return assignment_one_range, assignment_two_range
+
+def parse_direction(direction: str):
+    # take in input in the form "move X from Y to Z"
+    amount, start, end = 0, 0, 0
+
+    # pull variables
+    split_direction = direction.split(' ')
+    amount = int(split_direction[1])
+    start = int(split_direction[3]) - 1
+    end = int(split_direction[5]) - 1
+
+    return amount, start, end
 
 
 if __name__ == '__main__':
-    print(solve(1))
-    print(solve(2))
+    print(solve(9000))
+    print(solve(9001))
